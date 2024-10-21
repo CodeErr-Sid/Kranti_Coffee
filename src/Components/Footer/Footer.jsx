d7a082d6292e8dc7995898ed8cf0718b640b4281
Akram-00 <aliakram9789@gmail.com>
2024-10-21 21:52:44 +0530

    Footer is completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import assets from '../../assets/assets'
+import { FaFacebook, FaYoutube, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="features-section bg-primary flex items-center py-12 px-6 sm:px-8 md:px-16">
+      <div className="container flex flex-row items-center justify-between gap-4">
+        <div className="logo-section w-1/5">
+          <img src={assets.logo} alt="" />
+        </div>
+        <div className="navlinks flex-1">
+          <ul className='md:ml-8 text-secondary font-monteserrat font-semibold flex flex-col gap-2'>
+            <a href="#">
+              <li className="capitalize">About</li>
+            </a>
+            <a href="#">
+              <li className="capitalize">People Services</li>
+            </a>
+            <a href="#">
+              <li className="capitalize">Process Services</li>
+            </a>
+            <a href="#">
+              <li className="capitalize">Tech Solutions</li>
+            </a>
+            <a href="#">
+              <li className="capitalize">Contact us</li>
+            </a>
+          </ul>
+        </div>
+        <div className="social-media-section">
+          <button className='bg-white py-0.5 text-dark font-monteserrat font-semibold text-xl w-full rounded-md mb-4'>Get Services</button>
+          <div className="flex gap-4">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FaFacebook size={35} className="text-blue-600 bg-white rounded-full p-2" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <FaInstagram size={35} className="text-pink-500 bg-white rounded-full p-2" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <FaYoutube size={35} className="text-red-600 bg-white rounded-full p-2" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin size={35} className="text-blue-700 bg-white rounded-full p-2" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
