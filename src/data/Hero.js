084062711ec9b12222b16217f58734567271c82c
Akram-00 <aliakram9789@gmail.com>
2025-01-16 13:15:11 +0530

    Hero Image is changed

--- src/data/Hero.js
+++ src/data/Hero.js
@@ -6,7 +6,7 @@ const slidesContent = [
     {
         title: "Start Your Day Right with a Perfect Cup",
         content: "Experience the bold, authentic flavors of our premium coffee, carefully sourced from the finest farms.",
-        backgroundVideo: assets.coffeeVapourVideo, // Path to the image
+        backgroundImage: assets.krantiHero, // Path to the image
         titleClassName: 'mix-blend-difference',
         contentClassName: 'mix-blend-difference'
     },
